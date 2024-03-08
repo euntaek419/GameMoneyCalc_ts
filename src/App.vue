e034b835b5664952bd68fb8e95a23666103dd714
cet4713@gmail.com <cet4713@gmail.com>
2024-03-08 23:44:40 +0900

    타입스크립트 변환

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,79 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="wrap" ref="wrap">
+    <Title class="container"></Title>
+    <Main class="container"></Main>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup lang="ts">
+import Main from './components/Main.vue'
+import Title from './components/Title.vue'
+
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+@font-face{
+  font-family: 'MorganiteBold';
+  src : url('assets/fonts/MorganiteBold.woff');
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+@font-face{
+  font-family: 'NeurimboGothicRegular';
+  src : url('assets/fonts/neurimboGothicRegular.woff'), url('assets/fonts/MorganiteBold.woff');
+  font-display: fallback;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+@font-face{
+  font-family: 'PretendardMedium';
+  src : url('assets/fonts/PretendardMedium.woff'), url('assets/fonts/MorganiteBold.woff');
+  font-display: fallback;
+}
+
+@font-face{
+  font-family: 'PyeongChangPeaceBold';
+  src : url('assets/fonts/PyeongChangPeaceBold.woff'), url('assets/fonts/MorganiteBold.woff');
+  font-display: fallback;
+}
+
+@font-face{
+  font-family: 'PyeongChangPeaceLight';
+  src : url('assets/fonts/PyeongChangPeaceLight.woff'), url('assets/fonts/MorganiteBold.woff');
+  font-display: fallback;
+}
+
+@font-face{
+  font-family: 'Galmuri11';
+  src : url('assets/fonts/Galmuri11.woff'), url('assets/fonts/MorganiteBold.woff');
+  font-display: fallback;
+}
+
+
+* {
+  background-color: #000000;
+}
+
+/* 크롬 기본 스타일링 처리 */
+body {
+  padding: 0;
+  margin: 0;
+}
+
+.wrap {
+  position:absolute;
+  width:100%;
+  height:100%;
+  transition: 1s;
+}
+
+.container {
+  height:100%;
+}
+
+
+/* 수직 스크롤바를 숨김 */
+body {
+  overflow: hidden;
 }
 </style>
+
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import store from './store/store.d.js'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(store)
+.mount('#app')
